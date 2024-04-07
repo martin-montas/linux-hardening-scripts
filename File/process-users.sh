023a6c655b2322419c62f7d47ade411f956b62ab
Martin Montas <martinmontas1@gmail.com>
2024-04-07 19:04:28 -0400

    first commit

---
+++ File/process-users.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+#           process-users.sh is a Bash script that adds a private process to /etc/fstab
+#           It should be run as root.
+#
+#           Author: @eto330
+
+
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+FSTAB='/etc/fstab'
+PRIVATE_PROCESS='proc    /proc   proc    hidepid=2       0 0'
+
+
+if grep -Fxq "$PRIVATE_PROCESS" "$FSTAB"
+then
+    echo "Private process already in fstab"
+else
+    echo "Adding private process to fstab"
+    echo "$PRIVATE_PROCESS" >> "$FSTAB"
+    mount -o remount proc
+fi
+
+echo "Private process added to fstab"
