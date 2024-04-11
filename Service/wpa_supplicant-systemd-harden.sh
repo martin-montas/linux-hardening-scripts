c7548d5efce941c8baafdee3edf78e9880d44161
Martin Montas <martinmontas1@gmail.com>
2024-04-11 17:10:37 -0400

    latest file: wpa_supplicant hardening thru systemd drop-in file

---
+++ Service/wpa_supplicant-systemd-harden.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+#                    Service/wpa_supplicant-service-harden.sh
+#                    should be run as root
+#                    Author: @eto330
+#
+#                    this script adds config variables to the wpa_supplicant.service unit file
+#
+
+
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Please run as root."
+  exit 1
+fi
+
+SERVICE_NAME="wpa_supplicant.service"
+DROPIN_DIR="/etc/systemd/system/${SERVICE_NAME}.d"
+CONFIG_FILE="${DROPIN_DIR}/security.conf"
+
+
+mkdir -p "${DROPIN_DIR}"
+touch "${CONFIG_FILE}"
+
+[Service]
+# Restrict capabilities
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+
+# Set restrictive file system access
+ProtectSystem=full
+ProtectHome=yes
+NoNewPrivileges=yes
+ReadWritePaths=/etc/wpa_supplicant
+EOF
+
+systemctl daemon-reload
+systemctl restart  ${SERVICE_NAME}
+
+
